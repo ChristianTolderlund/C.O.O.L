8524148d47e031b1e49cac837711f96f18b7399b
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2021-09-27 01:21:20 +0200

    Created a template and appended url

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const url = "https://kea2021int-f4d0.restdb.io/rest/exhibition";
+const url = "https://kea2021int-f4d0.restdb.io/rest/artworks";
 
 // The API-key
 const options = {
@@ -7,19 +7,40 @@ const options = {
     },
 };
 
- fetch(url, options)
- .then((response) => {
-     if (!response.ok) {
-         throw Error(response.statusText);
-     }
-     return response.json();
- })
-.then((data) => {
-    //We have the data
-    console.log(data);
-    // handleData(data);
-})
-.catch((e) => {
-    //Woops, something went wrong
-    console.error("An error occured:", e.message);
-});
+fetch(url, options)
+    .then((response) => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        //We have the data
+        console.log(data);
+        handleData(data);
+    })
+    .catch((e) => {
+        //Woops, something went wrong
+        console.error("An error occured:", e.message);
+    });
+
+function handleData(data) {
+    data.forEach((artworks) => {
+        console.log(artworks);
+        // 1. make a template
+        // 2. grab it 
+        const template = document.querySelector(".archive_template").content;
+        // 3. clone it
+        const clone = template.cloneNode(true);
+        // 4. populate with data
+        clone.querySelector(".archive_tile_artwork").textContent = artworks.artwork_title;
+        clone.querySelector(".archive_tile_artist").textContent = artworks.artist_name;
+        clone.querySelector(".artwork_date").textContent = artworks.artwork_date;
+        clone.querySelector(".artwork_description").textContent = artworks.description;
+        clone.querySelector(".artwork_type").textContent = artworks.artwork_type;
+
+        // 5. append it to the DOM
+        const parent = document.querySelector(".archive_content");
+        parent.appendChild(clone);
+    });
+}
