ae5b6c6274aea640ebd76cd1905e8e8e67f1f0c4
ct <chri19jy@stud.kea.dk>
2021-09-27 14:22:45 +0200

    nav bar and css styling done

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
+    document.querySelector("#side-nav").style.display = "block";
     menuBtn.classList.add("open");
     menuOpen = true;
   } else {
+    document.querySelector("#side-nav").style.display = "none";
     menuBtn.classList.remove("open");
     menuOpen = false;
   }
