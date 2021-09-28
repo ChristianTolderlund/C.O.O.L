a6dfa1d4f79892523544aa6c744a87b5c60965c0
ct <chri19jy@stud.kea.dk>
2021-09-28 11:45:21 +0200

    nav bar load fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
+document.querySelector("#side-nav").style.display = "none";
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
     document.querySelector("#side-nav").style.display = "block";
